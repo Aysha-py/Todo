eacea36895dc9353aa0581b45f6e1462b36a513f
Aysha-py <m.aishat9890.github.com>
2022-01-01 04:14:35 +0100

    add

--- app.js
+++ app.js
@@ -16,6 +16,74 @@ lightMode.addEventListener("click",function(){
         
     }
     
-    
-
 })
+
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+  myNodelist[i].appendChild(span);
+}
+
+var close =document.getElementsByClassName("close");
+
+var i;
+
+for (i=0; i<=close.length; i++){
+    close[i].eonclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }  
+    
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+   console.log("hfhf")
+  }
+}, false);
+
+
+function newElement(){
+    var li = document.createElement('li')
+    var input =document.getElementById('typing').value;
+    var t = document.createTextNode (input)
+    li.appendChild(t);
+    if (input === ""){
+        alert ("enter a to-do item")
+    }
+    else{
+        document.getElementById("mylist").appendChild(li);
+    }
+    document.getElementById('typing').value = ""
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span)
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+          var div = this.parentElement;
+          div.style.display = "none";
+        }
+    }
+
+    // function countItems(){
+    //     var add = document.getElementById('typing').value;
+    //     var count = document.getElementById('count')
+    //     if (add){
+    //         var i,
+
+    //         for (i=0; i<=add; i++){
+    //             count.innerHTML = i + "items-left"
+    //         }
+    // }   }
+
+         
+}
