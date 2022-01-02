220897146b8f06b47559a6db9f9450931058d3c3
Aysha-py <m.aishat9890.github.com>
2022-01-02 04:19:36 +0100

    add

--- app.js
+++ app.js
@@ -1,60 +1,56 @@
 let lightMode = document.getElementById("icon");
-let bckImage= document.getElementById("bg-image");
+let bckImage = document.getElementById("bg-image");
 
-lightMode.addEventListener("click",function(){
-    if (document.body.style.backgroundColor === "black"){
+lightMode.addEventListener("click", function () {
+    if (document.body.style.backgroundColor === "black") {
 
-        document.body.style.backgroundColor= "white";
-        lightMode.innerHTML =`<i class="fas fa-moon"></i>`
-        bckImage.innerHTML=`<img src="img/bg-desktop-light.jpg" alt="">`
+        document.body.style.backgroundColor = "white";
+        lightMode.innerHTML = `<i class="fas fa-moon"></i>`
+        bckImage.innerHTML = `<img src="img/bg-desktop-light.jpg" alt="">`
 
     }
-    else{
-        document.body.style.backgroundColor= "black";
-        lightMode.innerHTML =`<i class="fas fa-sun"></i>`
-        bckImage.innerHTML=`<img src="img/bg-desktop-dark.jpg" alt="">`
-        
+    else {
+        document.body.style.backgroundColor = "black";
+        lightMode.innerHTML = `<i class="fas fa-sun"></i>`
+        bckImage.innerHTML = `<img src="img/bg-desktop-dark.jpg" alt="">`
+
     }
-    
+
 })
 
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-  myNodelist[i].appendChild(span);
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+    myNodelist[i].appendChild(span);
 }
 
 
-var close =document.getElementsByClassName("close");
-
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
 var i;
-
-for (i=0; i<=close.length; i++){
-    close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-    }  
-    
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
 }
 
 
 
-
-
-function newElement(){
+function newElement() {
     var li = document.createElement('li')
-    var input =document.getElementById('typing').value;
-    var t = document.createTextNode (input)
+    var input = document.getElementById('typing').value;
+    var t = document.createTextNode(input)
     li.appendChild(t);
-    if (input === ""){
-        alert ("enter a to-do item")
+    if (input === "") {
+        alert("enter a to-do item")
     }
-    else{
+    else {
         document.getElementById("mylist").appendChild(li);
     }
     document.getElementById('typing').value = ""
@@ -65,9 +61,9 @@ function newElement(){
     span.appendChild(txt);
     li.appendChild(span)
     for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-          var div = this.parentElement;
-          div.style.display = "none";
+        close[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
         }
     }
 
@@ -83,5 +79,5 @@ function newElement(){
     //         }
     // }   }
 
-         
+
 }
